7174669587c249ebcdd3af3171ea01f928a55f07
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2022-11-15 15:26:35 +0100

    change from log to return

--- main.js
+++ main.js
@@ -11,47 +11,46 @@ function getComputerChoice() {
     }
 }
 
-function play(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
     // format play selection
     playerSelection = playerSelection.toLowerCase();
-    // display selection
-    console.log(`Player selected ${playerSelection}`);
-    console.log(`Computer selected ${computerSelection}`);
     // Tie
     if (playerSelection == computerSelection) {
-        console.log("Tie!");
+        return "Tie!";
     }
     // Player selects Rock
     else if (playerSelection == "rock") {
         // Player wins, Rock beats Scissors
         if (computerSelection == "scissors") {
-            console.log("You Win! Rock beats Scissors");
+            return "You Win! Rock beats Scissors";
         }
         // Player loses, Paper beats Rock
         else {
-            console.log("You Lose! Paper beats Rock");
+            return "You Lose! Paper beats Rock";
         }
     }
     // Player selects Paper
     else if (playerSelection == "paper") {
         // Player wins, Paper beats Rock
         if (computerSelection == "rock") {
-            console.log("You Win! Paper beats Rock");
+            return "You Win! Paper beats Rock";
         }
         // Player loses, Scissors beats Paper
         else {
-            console.log("You Lose! Scissors beats Paper");
+            return "You Lose! Scissors beats Paper";
         }
     }
     // Player selects Scissors
     else if (playerSelection == "scissors") {
         // Player wins, Scissors beats Paper
         if (computerSelection == "paper") {
-            console.log("You Win! Scissors beats Paper");
+            return "You Win! Scissors beats Paper";
         }
         // Player loses, Rock beats Scissors
         else {
-            console.log("You Lose! Rock beats Scissors");
+            return "You Lose! Rock beats Scissors";
         }
     }
 }
+
+console.log(playRound("paper",getComputerChoice()));
